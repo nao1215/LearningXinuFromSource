2e46693a4297a412ae8554e644731989028261ea
Nao1215 <n.chika156@gmail.com>
2020-10-06 06:21:00 +0900

    追加：デバイスヘッダにDoxygenスタイルコメントを追加

--- include/device.h
+++ include/device.h
@@ -1,7 +1,16 @@
-/* device.h - isbaddev */
+/**
+ * @file device.h
+ * @brief 「デバイス名の最大長」と「デバイスID検証マクロ」を提供する。
+ */
 
-#define	DEVNAMLEN	16	/* maximum size of a device name	*/
+//!  デバイス名の最大サイズ
+#define DEVNAMLEN 16
 
 /* Macro used to verify device ID is valid  */
-
-#define isbaddev(f)  ( ((f) < 0) | ((f) >= NDEVS) )
+/**
+ * @def isbaddev()
+ * @brief デバイスIDを検証するマクロ。
+ * @param デバイスディスクリプタ
+ * @return デバイスIDが不正の場合は真（非0）、正常の場合は偽（0）を返す。
+ */
+#define isbaddev(f) (((f) < 0) | ((f) >= NDEVS))
