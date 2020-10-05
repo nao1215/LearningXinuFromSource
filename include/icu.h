3ade24423705f410848ef8a5b205adb2ee13d3c8
Nao1215 <n.chika156@gmail.com>
2020-10-06 06:38:25 +0900

    追加：icu.hにDoxygenスタイルコメントを追加

--- include/icu.h
+++ include/icu.h
@@ -1,9 +1,15 @@
-/* icu.h */
+/**
+ *  @file icu.h
+ *  @brief Input Capture UnitのI/Oポートアドレス、レジスタ、割り込み終了を定義する。
+ */
 
-#define	ICU1	0x20		/* I/O port address, 8259A #1		*/
-#define	ICU2	0xA0		/* I/O port address, 8258A #2		*/
-
-#define	OCR	ICU1		/* Operation Command Register		*/
-#define	IMR	(ICU1+1)	/* Interrupt Mask Register		*/
-
-#define	EOI	0x20		/* non-specific end of interrupt	*/
+//! I/Oポートアドレス（8259A #1）
+#define ICU1 0x20
+//! I/Oポートアドレス（8258A #2）
+#define ICU2 0xA0
+//! オペレーションコマンドレジスタ（Operation Command Register）
+#define OCR ICU1
+//! 割り込みマスクレジスタ（Interrupt Mask Register）
+#define IMR (ICU1 + 1)
+//! 非特定の割り込み終了
+#define EOI 0x20
