ae77ee50e7a64836111a4102129852e393ec48ab
Nao1215 <n.chika156@gmail.com>
2020-10-10 23:55:02 +0900

    追加：XINUシェルのexitコマンドに関するDoxygenコメントを追加

--- shell/xsh_exit.c
+++ shell/xsh_exit.c
@@ -1,12 +1,15 @@
-/* xsh_exit.c - xshexit */
+/**
+ * @file xsh_exit.c
+ * @brief XINUシェルにおけるexitコマンドを提供する。
+ * @details exitコマンドは、シェル終了を引き起こす終了コードを返す。
+ */
 
 #include <xinu.h>
 
-/*------------------------------------------------------------------------
- * xsh_exit  -  shell command returns the exit code causing shell exit
- *------------------------------------------------------------------------
+/**
+ * @brief exitコマンドとして、シェル終了を引き起こす終了コードを返す。
+ * @return SHELL_EXITを返す。
  */
-
 shellcmd xsh_exit(int nargs, char *args[])
 {
 	return SHELL_EXIT;
