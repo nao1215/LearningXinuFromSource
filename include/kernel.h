35f820e143aa7043066c8ee9b05ce526b407dd04
Nao1215 <n.chika156@gmail.com>
2020-10-10 22:00:22 +0900

    追加：「XINU OSのkernelで用いる汎用的な宣言」に関するDoxygenコメントを追加

--- include/kernel.h
+++ include/kernel.h
@@ -1,73 +1,103 @@
-/* kernel.h */
+/**
+ * @file kernel.h
+ * @brief XINU OSのkernel部分で使用する汎用的な宣言。
+ */
 
-/* General type declarations used throughout the kernel */
+//! 符号なし8ビット値（unsigned char）
+typedef unsigned char byte;
+//! 符号なし8ビット値（unsigned char）
+typedef unsigned char uint8;
+//! 符号あり32ビット整数（int）
+typedef int int32;
+//! 符号あり16ビット整数（short）
+typedef short int16;
+//! 符号なし32ビット整数（unsigned int）
+typedef unsigned int uint32;
+//! 符号なし16ビット整数（unsigned short）
+typedef unsigned short uint16;
+//! 符号なし64ビット整数（unsigned long long）
+typedef unsigned long long uint64;
 
-typedef	unsigned char	byte;
-typedef	unsigned char	uint8;
-typedef	int		int32;
-typedef	short		int16;
-typedef	unsigned int	uint32;
-typedef	unsigned short	uint16;
-typedef	unsigned long long uint64;
+//! セマフォID
+typedef int32 sid32;
+//! キューID
+typedef int16 qid16;
+//! プロセスID
+typedef int32 pid32;
+//! デバイスID
+typedef int32 did32;
+//! プロセス優先度
+typedef int16 pri16;
+//! プロセス間で渡されるメッセージ
+typedef uint32 umsg32;
+//! バッファプールID
+typedef int32 bpid32;
+//! Boolean値
+typedef byte bool8;
+//! 保存された割り込みマスク
+typedef uint32 intmask;
+//! ブロックIDのインデックス（ファイルシステムで使用する）
+typedef int32 ibid32;
+//! データブロックID（ファイルシステムで使用する）
+typedef int32 dbid32;
+//! UDPテーブルディスクリプタのID
+typedef int32 uid32;
 
-/* Xinu-specific types */
+//! システムコール関数 返り値の型
+typedef int32 syscall;
+//! デバイスコール関数 返り値の型
+typedef int32 devcall;
+//! シェルコール関数 返り値の型
+typedef int32 shellcmd;
+//! プロセスの最上位レベル関数 返り値の型
+typedef int32 process;
+//! 割り込みハンドラ 返り値の型
+typedef void interrupt;
+//! ステータスを意味する返り値の型（OK／SYSERR）
+typedef int32 status;
 
-typedef	int32	sid32;		/* semaphore ID				*/
-typedef	int16	qid16;		/* queue ID				*/
-typedef	int32	pid32;		/* process ID				*/
-typedef	int32	did32;		/* device ID				*/
-typedef	int16	pri16;		/* process priority			*/
-typedef	uint32	umsg32;		/* message passed among processes	*/
-typedef	int32	bpid32;		/* buffer pool ID			*/
-typedef	byte	bool8;		/* Boolean type				*/
-typedef	uint32	intmask;	/* saved interrupt mask			*/
-typedef	int32	ibid32;		/* index block ID (used in file system)	*/
-typedef	int32	dbid32;		/* data block ID (used in file system)	*/
-typedef	int32	uid32;		/* ID for UDP table descriptor		*/
+//! ローカル関数かローカル変数の宣言
+#define local static
 
-/* Function declaration return types */
+//! Boolean False（0）
+#define FALSE 0
+//! Boolean True（1）
+#define TRUE 1
 
-typedef int32	syscall;	/* system call declaration		*/
-typedef int32	devcall;	/* device call declaration		*/
-typedef int32	shellcmd;	/* shell command declaration		*/
-typedef int32	process;	/* top-level function of a process	*/
-typedef	void	interrupt;	/* interrupt procedure			*/
-typedef	int32	status;		/* returned status value (OK/SYSERR)	*/
-
-#define local	static		/* Local procedure or variable declar.	*/
-
-/* Boolean constants */
-
-#define FALSE	0		/* Boolean False			*/
-#define TRUE	1		/* Boolean True				*/
-
-/* Null pointer, character, and string definintions */
-
-#define NULL	0		/* null pointer for linked lists	*/
-#define NULLCH	'\0'		/* null character			*/
-#define	NULLSTR	""		/* null string				*/
+//! 連結リスト用のNULLポインタ
+#define NULL 0
+//! NULL文字（NULL終端）
+#define NULLCH '\0'
+//! 空文字
+#define NULLSTR ""
 
 /* Universal return constants */
 
-#define OK	( 1)		/* normal system call return		*/
-#define	SYSERR	(-1)		/* system call failed			*/
-#define	EOF	(-2)		/* End-of-file (usually from read)	*/
-#define	TIMEOUT	(-3)		/* system call timed out		*/
+//! 処理が成功した場合
+#define OK (1)
+//! 処理が失敗した場合
+#define SYSERR (-1)
+//! ファイルの終端（End of File）に達した場合（読み込み処理に用いる）
+#define EOF (-2)
+//! システムコールがタイムアウトした場合
+#define TIMEOUT (-3)
 
-extern	qid16	readylist;	/* global ID for list of ready processes*/
+//! READY状態のプロセスリストに対するグローバルID
+extern qid16 readylist;
 
-#define	MINSTK	400		/* minimum stack size in bytes		*/
+//! 最小のスタックサイズ（Byte）
+#define MINSTK 400
+//! ランタイムスタックの関数呼び出しコンテキスト上のByte
+#define CONTEXT 64
+//! ミリ秒単位のタイムスライス
+#define QUANTUM 2
 
-#define	CONTEXT	64		/* bytes in a function call context on	*/
-				/* the run-time stack			*/
-#define	QUANTUM	2		/* time slice in milliseconds		*/
+//! NULLプロセスのスタックサイズ
+#define NULLSTK 8192 /* stack size for null process		*/
 
-/* Size of the stack for the null process */
-
-#define	NULLSTK		8192	/* stack size for null process		*/
-
-/* Prototypes of I/O functions used throughout the kernel */
-
-syscall	kprintf(char *fmt, ...);
-syscall	kputc(byte);
-syscall	kgetc(void);
+//! ポーリングI/Oを使用して、フォーマットされた文字列をコンソールに出力する。
+syscall kprintf(char *fmt, ...);
+//! ポーリングI/Oを使用して、コンソールのシリアルラインに文字を書き込む。
+syscall kputc(byte);
+//! ポーリングI/Oを使用して、コンソールのシリアルラインから文字を読み取る。
+syscall kgetc(void);
