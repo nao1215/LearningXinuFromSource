a0a51fd6617ca58036b01f1905a3a6ef797a25ab
Nao1215 <n.chika156@gmail.com>
2020-10-10 21:12:25 +0900

    追加：優先度に基づいたキュー操作APIに関するDoxygenコメントを追加

--- system/insert.c
+++ system/insert.c
@@ -1,32 +1,41 @@
-/* insert.c - insert */
-
+/**
+ * @file insert.c
+ * @brief 優先度に基づいてキューにプロセスを挿入するAPIを提供する。
+ */
 #include <xinu.h>
 
-/*------------------------------------------------------------------------
- *  insert  -  Insert a process into a queue in descending key order
- *------------------------------------------------------------------------
+/**
+ * @brief 優先度に基づいて、キューにプロセスを挿入する。
+ * @details  優先度の降順に並べたキューに対して、以下を行う。
+ * Step1. キューを先頭から走査（whileループ部）<br>
+ * Step2. 挿入しようとしているプロセスIDの優先度より低い優先度を持つプロセスを探す。<br>
+ * Step3.「より低優先度なプロセスノード（Step2で見つけたノード）」と「その前にあるノード」との間に、
+ * 挿入したいプロセスを配置する。
+ * なお、キューの末尾は有効な最小キーを下回るため、ループは必ず停止する。
+ * @param[in] pid 挿入するプロセスID
+ * @param[in] q 使用するキューID
+ * @param[in] 挿入するプロセスの優先度（キー）
+ * @return 成功時はOK、キューIDもしくはプロセスIDが不正の場合はSYSERRを返す。
  */
-status	insert(
-	  pid32		pid,		/* ID of process to insert	*/
-	  qid16		q,		/* ID of queue to use		*/
-	  int32		key		/* Key for the inserted process	*/
-	)
+status insert(pid32 pid, qid16 q, int32 key)
 {
-	qid16	curr;			/* Runs through items in a queue*/
-	qid16	prev;			/* Holds previous node index	*/
+	qid16 curr; /* Runs through items in a queue*/
+	qid16 prev; /* Holds previous node index	*/
 
-	if (isbadqid(q) || isbadpid(pid)) {
+	if (isbadqid(q) || isbadpid(pid))
+	{
 		return SYSERR;
 	}
 
 	curr = firstid(q);
-	while (queuetab[curr].qkey >= key) {
+	while (queuetab[curr].qkey >= key)
+	{
 		curr = queuetab[curr].qnext;
 	}
 
 	/* Insert process between curr node and previous node */
 
-	prev = queuetab[curr].qprev;	/* Get index of previous node	*/
+	prev = queuetab[curr].qprev; /* Get index of previous node	*/
 	queuetab[pid].qnext = curr;
 	queuetab[pid].qprev = prev;
 	queuetab[pid].qkey = key;
