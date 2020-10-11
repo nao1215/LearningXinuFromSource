de9430682f290ce2dd54b18e7f8e58ac1ab6ff9e
Nao1215 <n.chika156@gmail.com>
2020-10-11 20:34:55 +0900

    修正：ファイルの最後に空行がなかったため、DoxygenがWarningを出力していた。この問題を修正。

--- include/device.h
+++ include/device.h
@@ -6,11 +6,10 @@
 //!  デバイス名の最大サイズ
 #define DEVNAMLEN 16
 
-/* Macro used to verify device ID is valid  */
 /**
  * @def isbaddev()
  * @brief デバイスIDを検証するマクロ。
- * @param デバイスディスクリプタ
+ * @param[in] f デバイスディスクリプタ
  * @return デバイスIDが不正の場合は真（非0）、正常の場合は偽（0）を返す。
  */
 #define isbaddev(f) (((f) < 0) | ((f) >= NDEVS))
