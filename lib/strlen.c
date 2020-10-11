a0f0bd19537530211f97e733575f1e5b1021c8a6
Nao1215 <n.chika156@gmail.com>
2020-10-11 20:36:05 +0900

    追加：文字列の長さを取得するAPI（strlen()）のDoxygenコメントを追加

--- lib/strlen.c
+++ lib/strlen.c
@@ -1,20 +1,22 @@
-/* strlen.c - strlen */
-
-/*------------------------------------------------------------------------
- * strlen - Compute the length of a null-terminated character string, not
- *			counting the null byte.
- *------------------------------------------------------------------------
+/**
+ * @file strlen.c
+ * @brief NULL終端された文字列の長さを返す。NULL終端は長さに含まない。
  */
-int	strlen(
-	  char		*str		/* string to use		*/
-	)
+
+/**
+ * @brief NULL終端された文字列の長さを返す。NULL終端は長さに含まない。
+ * @param[in] str 長さを調べる対象の文字列
+ * @return  文字列の長さを返す。NULL終端は長さに含まない。
+ */
+int strlen(char *str)
 {
-	int	len;
+	int len;
 
 	len = 0;
 
-	while(*str++ != '\0') {
+	while (*str++ != '\0')
+	{
 		len++;
 	}
-	return  len;
+	return len;
 }
