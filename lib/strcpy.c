d496cf10c8085765babd3a6106ddaa2e361bfdaa
Nao1215 <n.chika156@gmail.com>
2020-10-12 06:44:15 +0900

    追加：strcpy()のAPI説明コメントおよび最新のDoxygenドキュメントを追加

--- lib/strcpy.c
+++ lib/strcpy.c
@@ -1,15 +1,18 @@
-/* strcpy.c - strcpy */
-
-/*------------------------------------------------------------------------
- * strcpy - Copy the string given by the second argument into the first
- *------------------------------------------------------------------------
+/**
+ * @file strcpy.c
+ * @brief 文字列Aに文字列Bをコピーする。
  */
-int	strcpy(
-	  char		*tar,		/* target string		*/
-	  char		*src		/* source string		*/
-	)
+
+/**
+ * @brief 文字列Aに文字列B（NULL終端を含めて）をコピーする。
+ * @param[in,out] tar コピー先の文字列
+ * @param[in] src コピーする文字列
+ * @return 0を返す。
+ */
+int strcpy(char *tar, char *src)
 {
-	while ( (*tar++ = *src++) != '\0') {
+	while ((*tar++ = *src++) != '\0')
+	{
 		;
 	}
 	return 0;
