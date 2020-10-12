2455940b192dbf0b79146274810ac7baec2ae990
Nao1215 <n.chika156@gmail.com>
2020-10-13 06:46:15 +0900

    追加：abs()、labs()の仕様に関するDoxygenコメントを追加

--- lib/labs.c
+++ lib/labs.c
@@ -1,12 +1,14 @@
-/* labs.c - labs */
-
-/*------------------------------------------------------------------------
- *  labs  -  Calculates the absolute value of a long.
- *------------------------------------------------------------------------
+/**
+ * @file labs.c
+ * @brief 実数（long型）の絶対値を返す。
  */
-long	labs(
-	  long		arg
-	)
+
+/**
+ * @brief 実数（long型）の絶対値を返す。
+ * @param[in] arg 絶対値に変換したい実数（long型）
+ * @return 引数argの絶対値（long型）
+ */
+long labs(long arg)
 {
     if (arg < 0)
         arg = -arg;
