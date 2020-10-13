3526ab2f46f970bc599936008449dbe8c608418a
Nao1215 <n.chika156@gmail.com>
2020-10-13 20:09:06 +0900

    追加：bzero()に関するDoxygenコメントを追加

--- lib/bzero.c
+++ lib/bzero.c
@@ -1,13 +1,17 @@
-/* bzero.c - bzero */
-
-/*------------------------------------------------------------------------
- *  bzero  -  Clears a block of characters to 0s.
- *------------------------------------------------------------------------
+/**
+ * @file bzero.c
+ * @brief Byteブロック領域の先頭N bytesを数値ゼロで埋める。
  */
-void	bzero(
-	  void		*p,
-	  int		len
-	)
+
+/**
+ * @brief Byteブロック領域の先頭N bytesを数値ゼロで埋める。
+ * @param[in,out] p Byteブロック（bzero()終了後にゼロで埋まった状態となる）
+ * @param[in] len 数値ゼロ埋めするサイズ（Byte）
+ * @note 引数lenが0以下の場合は、何もせずreturnする。
+ */
+void bzero(
+    void *p,
+    int len)
 {
     int n;
     char *pch = (char *)p;
@@ -19,6 +23,5 @@ void	bzero(
     do
     {
         *pch++ = 0;
-    }
-    while (--n);
+    } while (--n);
 }
