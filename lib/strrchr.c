01bf130f3473b67f6d5c3358e10f517790a697d4
Nao1215 <n.chika156@gmail.com>
2020-10-13 21:48:11 +0900

    追加：strrchr()に関するDoxygenコメントを追加

--- lib/strrchr.c
+++ lib/strrchr.c
@@ -1,15 +1,16 @@
-/* strrchr.c - strrchr */
-
-/*------------------------------------------------------------------------
- *  strrchr  -  Return a pointer to the location in a string at which a
- *			particular character last appears or NULL if the
- *			char is not found
- *------------------------------------------------------------------------
+/**
+ * @file strrchr.c
+ * @brief 文字列から指定された文字を探し、最後に見つかった位置をポインタで返す。
  */
-char	*strrchr(
-	  const char	*s,		/* String to search		*/
-	  int		c		/* Character to locate		*/
-	)
+
+/**
+ * @brief 文字列の後方から指定された文字を探し、最後に見つかった位置をポインタで返す。
+ * @param[in] s 探索対象の文字列
+ * @param[in] c 検索文字
+ * @return 文字cが見つかった場合は文字cが最後に登場した位置を示すポインタ、<br>
+ * 文字cが見つからなかった場合はNULLを返す。
+ */
+char *strrchr(const char *s, int c)
 {
     char *r = 0;
 
