6bd8bc598ce88bc2482f1a3deec500b231154fe2
Nao1215 <n.chika156@gmail.com>
2020-10-14 20:19:58 +0900

    追加：strchr()に関するDoxygenコメントを追加

--- lib/strchr.c
+++ lib/strchr.c
@@ -1,14 +1,16 @@
-/* strchr.c - strchr */
-
-/*------------------------------------------------------------------------
- *  strchr  -  Returns a pointer to the location in a string at which a
- *			   character appears or NULL if char not found
- *------------------------------------------------------------------------
+/**
+ * @file strchr.c
+ * @brief 指定された文字を文字列から探し、最初にに見つかった位置をポインタで返す。
  */
-char	*strchr(
-	  const char	*s,		/* String to search		*/
-	  int		c		/* Character to locate		*/
-	)
+
+/**
+ * @brief 指定された文字を文字列から探し、最初にに見つかった位置をポインタで返す。
+ * @param[in] s 探索対象の文字列
+ * @param[in] c 検索文字
+ * @return 文字cが見つかった場合は文字cが最初に登場した位置を示すポインタ、<br>
+ * 文字cが見つからなかった場合はNULLを返す。
+ */
+char *strchr(const char *s, int c)
 {
     for (; *s != '\0'; s++)
     {
