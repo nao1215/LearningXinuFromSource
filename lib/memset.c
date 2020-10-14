a291d35e56e80c4a66964b087e0f3e735373e726
Nao1215 <n.chika156@gmail.com>
2020-10-15 06:32:22 +0900

    追加：memset()に関するDoxygenコメントを追加

--- lib/memset.c
+++ lib/memset.c
@@ -1,15 +1,20 @@
-/* memset.c - memset */
-
-/*------------------------------------------------------------------------
- *  memset  -  Set a block ot n bytes to the same value and return a
- *			   pointer to the memory
- *------------------------------------------------------------------------
+/**
+ * @file memset.c
+ * @brief 指定のByteブロックに対して、同じ値をNバイト分書き込む。
  */
-void	*memset(
-	  void		*s,		/* Address of memory block	*/
-	  int		c,		/* Byte value to use		*/
-	  int		n		/* Size of block in bytes 	*/
-	)
+
+/**
+ * @brief 指定のByteブロックに対して、同じ値をNバイト分書き込む。
+ * @param[in,out] s Byteブロックへのポインタ（例：文字列）
+ * @param[in] c 書き込む値（1Byte）
+ * @param[in] n 書き込むサイズ（Byte）
+ * @return 書き込み完了後のByteブロックへのポインタ
+ */
+void *memset(
+    void *s, /* Address of memory block	*/
+    int c,   /* Byte value to use		*/
+    int n    /* Size of block in bytes 	*/
+)
 {
     register int i;
     char *cp = (char *)s;
