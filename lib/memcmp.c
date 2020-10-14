4596e27eb00972d1f3e44456d434c1a17118579f
Nao1215 <n.chika156@gmail.com>
2020-10-15 06:47:21 +0900

    追加：memcmp()に関するDoxygenコメントを追加

--- lib/memcmp.c
+++ lib/memcmp.c
@@ -1,4 +1,7 @@
-/* memcmp.c - memcmp */
+/**
+ * @file memcmp.c
+ * @brief Byteブロック2個に対して、先頭からN Byte分比較する。
+ */
 
 /*------------------------------------------------------------------------
  *  memcmp  -  Compare two equal-size blocks of memory.  If there are no
@@ -6,11 +9,14 @@
  *			if the first differing byte is greater or less
  *------------------------------------------------------------------------
  */
-int	memcmp(
-	  const void	*s1,		/* first memory location	*/
-	  const void	*s2,		/* second memory location	*/
-	  int		n		/* length to compare		*/
-	)
+/**
+ * @brief Byteブロック2個に対して、先頭からN Byte分比較する。
+ * @param[in] s1 Byteブロックその1
+ * @param[in] s2 Byteブロックその2
+ * @param[in] n 比較するサイズ（Byte）
+ * @return Byteブロックs1とs2に差がなければ0、s1>s2の場合は正の値、s1<s2の場合は負の値を返す。
+ */
+int memcmp(const void *s1, const void *s2, int n)
 {
     const unsigned char *c1;
     const unsigned char *c2;
