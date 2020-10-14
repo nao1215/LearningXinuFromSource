29b7ef3b973ca14f96189fd9e9a590dbd4dcac24
Nao1215 <n.chika156@gmail.com>
2020-10-14 21:40:12 +0900

    追加：rand()に関するDoxygenコメントおよび最新Doxygenドキュメントを追加

--- lib/rand.c
+++ lib/rand.c
@@ -1,38 +1,41 @@
-/* rand.c - rand, rand_r, srand */
-
+/**
+ * @file rand.c
+ * @brief 疑似乱数を返す。
+ */
 #include <stdlib.h>
 
+//! 疑似乱数のシード値。
 static unsigned int rand_seed = 1;
-extern	int rand_r(unsigned int*);
+//! 任意のシード値を用いて、疑似乱数を返すrand_r()の宣言。
+extern int rand_r(unsigned int *);
 
-/*------------------------------------------------------------------------
- *  rand  -  Calculates a uniform random number [0, RAND_MAX]
- *------------------------------------------------------------------------
+/**
+ * @brief 予め設定済みのシード値を用いて、疑似乱数を返す。
+ * @details シード値を変えない限り、疑似乱数に再現性がある。
+ * @return 疑似乱数（int型の整数値。0〜RAND_MAXの範囲で表される値）
  */
-int	rand()
+int rand()
 {
-    return rand_r(&rand_seed);
+	return rand_r(&rand_seed);
 }
 
-/*------------------------------------------------------------------------
- *  rand_r  -  Calculates a uniform random number [0, RAND_MAX]
- *------------------------------------------------------------------------
+/**
+ * @brief 任意のシード値を用いて、疑似乱数を返す。
+ * @param[in,out] seedp 疑似乱数のシード値（乱数を生成する際に用いる値）
+ * @return 疑似乱数（int型の整数値。0〜RAND_MAXの範囲で表される値）
  */
-int	rand_r(
-	  unsigned int*		seedp
-	)
+int rand_r(unsigned int *seedp)
 {
 	*seedp = 16807 * (*seedp) % (RAND_MAX + 1);
 	return (int)(*seedp);
 }
 
-/*------------------------------------------------------------------------
- *  srand  -  Set the seed for rand
- *------------------------------------------------------------------------
+/**
+ * @brief 疑似乱数のシード値を設定する。
+ * @param[in] seed 疑似乱数のシード（乱数を生成する際に用いる値）
  */
-void	srand(
-	  unsigned int		seed
-	)
+void srand(
+	unsigned int seed)
 {
-    rand_seed = seed;
+	rand_seed = seed;
 }
