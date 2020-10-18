1ce0601e1119b7860acc6d4427190652758fa9c8
Nao1215 <n.chika156@gmail.com>
2020-10-18 21:59:26 +0900

    追加：ready()に関するDoxygenコメントを追加

--- system/ready.c
+++ system/ready.c
@@ -1,20 +1,31 @@
-/* ready.c - ready */
+/**
+ * @file ready.c
+ * @brief プロセスをCPUサービスの対象にする。
+ */
 
 #include <xinu.h>
 
-qid16	readylist;			/* Index of ready list		*/
+//! READYリストのインデックス
+qid16 readylist;
 
-/*------------------------------------------------------------------------
- *  ready  -  Make a process eligible for CPU service
- *------------------------------------------------------------------------
+/**
+ * @brief プロセスをCPUサービスの対象にする。
+ * @details
+ * Step1. PIDが正常値かどうかを確認する。<br>
+ * Step2. プロセス情報を取得し、ステータスをREADY状態に移行する。<br>
+ * Step3. プロセスをREADYリストに挿入する。<br>
+ * Step4. 再スケジューリングを行う。<br>
+ * @param[in] pid READY状態に遷移させるプロセスのID
+ * @return プロセスがREADY状態となった場合はOK、引数が不正なPIDの場合はSYSERRを返す。
  */
-status	ready(
-	  pid32		pid		/* ID of process to make ready	*/
-	)
+status ready(
+	pid32 pid /* ID of process to make ready	*/
+)
 {
 	register struct procent *prptr;
 
-	if (isbadpid(pid)) {
+	if (isbadpid(pid))
+	{
 		return SYSERR;
 	}
 
