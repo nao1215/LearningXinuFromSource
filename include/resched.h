2211fb3c09b89d4a93fd1c9a3e39c811cf35cfe1
Nao1215 <n.chika156@gmail.com>
2020-10-20 15:07:37 +0900

    追加：遅延スケジューリングに関するDoxygenコメントを追加

--- include/resched.h
+++ include/resched.h
@@ -1,16 +1,25 @@
-/* resched.h */
 
-/* Constants and variables related to deferred rescheduling */
+/**
+ * @file resched.h
+ * @brief 遅延スケジューリングに使用する宣言と定数。
+ */
 
-#define	DEFER_START	1	/* Start deferred rescehduling		*/
-#define	DEFER_STOP	2	/* Stop  deferred rescehduling		*/
+//! 遅延リスケジューリングの開始
+#define DEFER_START 1
+//! 遅延リスケジューリングの停止
+#define DEFER_STOP 2
 
-/* Structure that collects items related to deferred rescheduling	*/
-
-struct	defer	{
-	int32	ndefers;	/* Number of outstanding defers 	*/
-	bool8	attempt;	/* Was resched called during the	*/
-				/*   deferral period?			*/
+/**
+ * @struct defer
+ * @brief 遅延リスケジューリングに関連する項目を集約した構造体
+ */
+struct defer
+{
+	//! 未処理の延期プロセスの数
+	int32 ndefers;
+	//! 延期中に再スケジューリングが実行されたかどうか
+	bool8 attempt;
 };
 
-extern	struct	defer	Defer;
+//! defer構造体のextern宣言
+extern struct defer Defer;
