24807a02cbd3faa8aa2bf340d5e4d452ce838f2e
Nao1215 <n.chika156@gmail.com>
2020-10-21 00:07:26 +0900

    追加：chprio()に関するDoxygenコメントを追加。

--- system/chprio.c
+++ system/chprio.c
@@ -1,24 +1,31 @@
-/* chprio.c - chprio */
-
+/**
+ * @file chprio.c
+ * @brief プロセスのスケジューリング優先度を変更する。
+ */
 #include <xinu.h>
 
-/*------------------------------------------------------------------------
- *  chprio  -  Change the scheduling priority of a process
- *------------------------------------------------------------------------
+/**
+ * @brief プロセスのスケジューリング優先度を変更する。
+ * @details
+ * Step1. 割り込みを禁止する。<br>
+ * Step2. 不正なPIDの場合は割り込み許可状態に戻し、処理を終了する。
+ * Step3. 引数で指定されたPIDからプロセス情報を取得し、新しい優先度に変更する。
+ * Step4. 割り込み許可状態に戻し、処理を終了する。
+ * @param[in] pid 優先度を変更したいプロセスのID
+ * @param[in] newprio 新しい優先度
+ * @return 優先度が変更できた場合は古い優先度、PIDが不正な場合はSYSERRを返す。
  */
-pri16	chprio(
-	  pid32		pid,		/* ID of process to change	*/
-	  pri16		newprio		/* New priority			*/
-	)
+pri16 chprio(pid32 pid, pri16 newprio)
 {
-	intmask	mask;			/* Saved interrupt mask		*/
-	struct	procent *prptr;		/* Ptr to process's table entry	*/
-	pri16	oldprio;		/* Priority to return		*/
+	intmask mask;		   /* Saved interrupt mask		*/
+	struct procent *prptr; /* Ptr to process's table entry	*/
+	pri16 oldprio;		   /* Priority to return		*/
 
 	mask = disable();
-	if (isbadpid(pid)) {
+	if (isbadpid(pid))
+	{
 		restore(mask);
-		return (pri16) SYSERR;
+		return (pri16)SYSERR;
 	}
 	prptr = &proctab[pid];
 	oldprio = prptr->prprio;
