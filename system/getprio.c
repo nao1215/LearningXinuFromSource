2fdd92bbf009fdda4be2f58e39a58283992240a3
Nao1215 <n.chika156@gmail.com>
2020-10-21 00:13:17 +0900

    追加：getprio()に関するDoxygenコメントを追加（chprio()の改行忘れも修正）

--- system/getprio.c
+++ system/getprio.c
@@ -1,20 +1,27 @@
-/* getprio.c - getprio */
-
+/**
+ * @file getprio.c
+ * @brief プロセスのスケジューリング優先度を取得する。
+ */
 #include <xinu.h>
 
-/*------------------------------------------------------------------------
- *  getprio  -  Return the scheduling priority of a process
- *------------------------------------------------------------------------
+/**
+ * @brief プロセスのスケジューリング優先度を取得する。
+ * @details
+ * Step1. 割り込みを禁止する。<br>
+ * Step2. 不正なPIDの場合は割り込み許可状態に戻し、処理を終了する。<br>
+ * Step3. 引数で指定されたPIDからプロセス情報を取得し、そこから優先度を取得する。<br>
+ * Step4. 割り込み許可状態に戻し、処理を終了する。<br>
+ * @param[in] pid 優先度を取得したいプロセスのID
+ * @return 処理が成功した場合は引数で指定したPIDの優先度、PIDが不正な場合はSYSERRを返す。
  */
-syscall	getprio(
-	  pid32		pid		/* Process ID			*/
-	)
+syscall getprio(pid32 pid)
 {
-	intmask	mask;			/* Saved interrupt mask		*/
-	uint32	prio;			/* Priority to return		*/
+	intmask mask; /* Saved interrupt mask		*/
+	uint32 prio;  /* Priority to return		*/
 
 	mask = disable();
-	if (isbadpid(pid)) {
+	if (isbadpid(pid))
+	{
 		restore(mask);
 		return SYSERR;
 	}
