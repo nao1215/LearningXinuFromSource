e7fccd7b1d62c7c786ede4fda15248d27b9b9baf
Nao1215 <n.chika156@gmail.com>
2020-10-20 16:01:46 +0900

    追加：再スケジューリングに関するDoxygenコメントを追加

--- system/resched.c
+++ system/resched.c
@@ -1,21 +1,33 @@
-/* resched.c - resched, resched_cntl */
-
+/**
+ * @file resched.c
+ * @brief プロセスを再スケジューリングする。
+ */
 #include <xinu.h>
 
-struct	defer	Defer;
+//! 遅延リスケジューリングに関連する項目を集約した構造体
+struct defer Defer;
 
-/*------------------------------------------------------------------------
- *  resched  -  Reschedule processor to highest priority eligible process
- *------------------------------------------------------------------------
+/**
+ * @brief 最優先の適切なプロセスにCPU実行権を渡す。
+ * @details resched()は、割り込みが禁止された区間で使用される事を想定している。<br>
+ * Step1. 再スケジューリングを遅延させられている場合、再スケジュールを試みた事を記録して終了する。<br>
+ * Step2. カレント（古い）プロセスのプロセステーブルを取得する<br>
+ * Step3. 「カレントプロセスが現在動作中」かつ「READYリスト先頭プロセスより高優先度」の場合は終了する。<br>
+ * Step4. カレントプロセスの状態を実行中からREADY状態に遷移させ、READYリストに挿入する。<br>
+ * Step5. カレントPIDをREADYリストの先頭プロセスとし、そのプロセスをREADY状態から実行状態に遷移させる。<br>
+ * Step6. プリエンプション（実行中のタスクを一時的に中断する動作）のためのタイムスライスを設定する。<br>
+ * Step7. 古いプロセスから新しいプロセスへコンテキストスイッチを行う。<br>
+ * Step8. 古いプロセスはresume()後に、resched()を即座にリターンする。
  */
-void	resched(void)		/* Assumes interrupts are disabled	*/
+void resched(void)
 {
-	struct procent *ptold;	/* Ptr to table entry for old process	*/
-	struct procent *ptnew;	/* Ptr to table entry for new process	*/
+	struct procent *ptold; /* Ptr to table entry for old process	*/
+	struct procent *ptnew; /* Ptr to table entry for new process	*/
 
 	/* If rescheduling is deferred, record attempt and return */
 
-	if (Defer.ndefers > 0) {
+	if (Defer.ndefers > 0)
+	{
 		Defer.attempt = TRUE;
 		return;
 	}
@@ -24,8 +36,10 @@ void	resched(void)		/* Assumes interrupts are disabled	*/
 
 	ptold = &proctab[currpid];
 
-	if (ptold->prstate == PR_CURR) {  /* Process remains eligible */
-		if (ptold->prprio > firstkey(readylist)) {
+	if (ptold->prstate == PR_CURR)
+	{ /* Process remains eligible */
+		if (ptold->prprio > firstkey(readylist))
+		{
 			return;
 		}
 
@@ -40,7 +54,7 @@ void	resched(void)		/* Assumes interrupts are disabled	*/
 	currpid = dequeue(readylist);
 	ptnew = &proctab[currpid];
 	ptnew->prstate = PR_CURR;
-	preempt = QUANTUM;		/* Reset time slice for process	*/
+	preempt = QUANTUM; /* Reset time slice for process	*/
 	ctxsw(&ptold->prstkptr, &ptnew->prstkptr);
 
 	/* Old process returns here when resumed */
@@ -52,29 +66,43 @@ void	resched(void)		/* Assumes interrupts are disabled	*/
  *  resched_cntl  -  Control whether rescheduling is deferred or allowed
  *------------------------------------------------------------------------
  */
-status	resched_cntl(		/* Assumes interrupts are disabled	*/
-	  int32	defer		/* Either DEFER_START or DEFER_STOP	*/
-	)
+/**
+ * @brief 再スケジューリングを延期させるか、もしくは許可させるかを制御する。
+ * @param[in] defer DEFER_START（延期開始）か、DEFER_STOP（延期終了）を指定する。
+ * @details resched_cntl()は、割り込みが禁止された区間で使用される事を想定している。<br>
+ * 引数がDEFER_STARTの場合、以下を実行後にOKを返して終了する。<br>
+ * 　・延期されたプロセス数が0の場合は、その数を1に変更<br>
+ * 　・延期されたプロセス数が0の場合は、延期中の再スケジューリングは未実施に変更<br>
+ * 引数がDEFER_STOPの場合、以下を実行する。<br>
+ * 　・延期されたプロセス数が負の値の場合は、SYSERRを返す。<br>
+ * 　・「延期されたプロセスが1の場合」かつ「延期中に再スケジューリング実施済み」の場合は再スケジューリングを実施し、OKを返す。
+ * 引数が不正な値の場合は、SYSERRを返す。
+ */
+status resched_cntl(int32 defer)
 {
-	switch (defer) {
+	switch (defer)
+	{
 
-	    case DEFER_START:	/* Handle a deferral request */
+	case DEFER_START: /* Handle a deferral request */
 
-		if (Defer.ndefers++ == 0) {
+		if (Defer.ndefers++ == 0)
+		{
 			Defer.attempt = FALSE;
 		}
 		return OK;
 
-	    case DEFER_STOP:	/* Handle end of deferral */
-		if (Defer.ndefers <= 0) {
+	case DEFER_STOP: /* Handle end of deferral */
+		if (Defer.ndefers <= 0)
+		{
 			return SYSERR;
 		}
-		if ( (--Defer.ndefers == 0) && Defer.attempt ) {
+		if ((--Defer.ndefers == 0) && Defer.attempt)
+		{
 			resched();
 		}
 		return OK;
 
-	    default:
+	default:
 		return SYSERR;
 	}
 }
