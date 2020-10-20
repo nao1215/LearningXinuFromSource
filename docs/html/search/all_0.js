80447ba7891728c26e2a30b741131f91498aa2ad
Nao1215 <n.chika156@gmail.com>
2020-10-20 13:33:28 +0900

    追加：restore()に関するドキュメントを追加

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -3,7 +3,9 @@ var searchData=
   ['00_2darm32bit_2emd',['00-arm32bit.md',['../d1/dad/00-arm32bit_8md.html',1,'']]],
   ['01_2dctxsw_2emd',['01-ctxsw.md',['../d7/dc2/01-ctxsw_8md.html',1,'']]],
   ['02_2dirq_2denable_5fdisable_2emd',['02-IRQ-enable_disable.md',['../d4/df2/02-IRQ-enable__disable_8md.html',1,'']]],
+  ['03_2drestore_2emd',['03-restore.md',['../de/d40/03-restore_8md.html',1,'']]],
   ['00_2darm32bit',['00-arm32bit',['../d0/d6e/md_docs_markdown_assembly_00-arm32bit.html',1,'']]],
   ['01_2dctxsw',['01-ctxsw',['../d3/d0a/md_docs_markdown_assembly_01-ctxsw.html',1,'']]],
-  ['02_2dirq_2denable_5fdisable',['02-IRQ-enable_disable',['../df/d00/md_docs_markdown_assembly_02-IRQ-enable_disable.html',1,'']]]
+  ['02_2dirq_2denable_5fdisable',['02-IRQ-enable_disable',['../df/d00/md_docs_markdown_assembly_02-IRQ-enable_disable.html',1,'']]],
+  ['03_2drestore',['03-restore',['../df/d53/md_docs_markdown_assembly_03-restore.html',1,'']]]
 ];
--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['00_2darm32bit',['00-arm32bit',['../d0/d6e/md_docs_markdown_assembly_00-arm32bit.html',1,'']]],
   ['01_2dctxsw',['01-ctxsw',['../d3/d0a/md_docs_markdown_assembly_01-ctxsw.html',1,'']]],
-  ['02_2dirq_2denable_5fdisable',['02-IRQ-enable_disable',['../df/d00/md_docs_markdown_assembly_02-IRQ-enable_disable.html',1,'']]]
+  ['02_2dirq_2denable_5fdisable',['02-IRQ-enable_disable',['../df/d00/md_docs_markdown_assembly_02-IRQ-enable_disable.html',1,'']]],
+  ['03_2drestore',['03-restore',['../df/d53/md_docs_markdown_assembly_03-restore.html',1,'']]]
 ];
