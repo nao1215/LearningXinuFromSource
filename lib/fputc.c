280eda184464d8295ac05c1010ff7c4c596192f2
Nao1215 <n.chika156@gmail.com>
2020-10-21 13:39:13 +0900

    追加：fputc()のDoxygenコメントを追加

--- lib/fputc.c
+++ lib/fputc.c
@@ -1,23 +1,27 @@
-/* fputc.c - fputc */
+/**
+ * @file fputc.c
+ * @brief 文字をデバイス（ファイル）に書き込む。
+ */
 
+//! 文字を書き込むputc()のextern宣言
 extern int putc(int, char);
 
-#ifndef	SYSERR
-#define	SYSERR   (-1)
+#ifndef SYSERR
+//! システムエラーを示す値
+#define SYSERR (-1)
 #endif
-#ifndef	EOF
-#define EOF      (-2)
+#ifndef EOF
+//! End Of Fileを示す値
+#define EOF (-2)
 #endif
 
-/*------------------------------------------------------------------------
- *  fputc  -  Write a character string to a device (file).
- *			  Return character written, EOF if error.
- *------------------------------------------------------------------------
+/**
+ * @brief 文字をデバイス（ファイル）に書き込む。
+ * @param[in] c 書き込む文字
+ * @param[in] dev デバイスディスクリプタ
+ * @return 成功時は書き込んだ文字、異常時はEOFを返す。
  */
-int	fputc(
-	  int		c,		/* char to write		*/
-	  int		dev		/* device to use		*/
-	)
+int fputc(int c, int dev)
 {
     if (SYSERR == (int)putc(dev, c))
     {
