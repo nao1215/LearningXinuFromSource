7a91064f7797c97923e670341db68c07e77312e8
Nao1215 <n.chika156@gmail.com>
2020-10-21 19:33:07 +0900

    追加：userret()に関するDoxygenコメントを追加

--- system/userret.c
+++ system/userret.c
@@ -1,12 +1,14 @@
-/* userret.c - userret */
-
+/**
+ * @file userret.c
+ * @brief プロセスが最上位の関数に戻る時に呼ばれる。現在のプロセスを終了させる。
+ */
 #include <xinu.h>
 
-/*------------------------------------------------------------------------
- *  userret  -  Called when a process returns from the top-level function
- *------------------------------------------------------------------------
+/**
+ * @brief プロセスが最上位の関数に戻る時に呼ばれる。現在のプロセスを終了させる。
+ * @note プロセス作成時に、戻りアドレスとしてuserret()がスタックに積まれる。
  */
-void	userret(void)
+void userret(void)
 {
-	kill(getpid());			/* Force process to exit */
+	kill(getpid()); /* Force process to exit */
 }
