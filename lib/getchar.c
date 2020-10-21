fcdcc7d10664671f757f9b86fa99befe1a48ddd0
Nao1215 <n.chika156@gmail.com>
2020-10-21 13:54:44 +0900

    追加：getchar()に関するDoxygenコメントを追加

--- lib/getchar.c
+++ lib/getchar.c
@@ -1,15 +1,15 @@
-/* getchar.c - getchar */
-
+/**
+ * @file getchar.c
+ * @brief STDIN（標準入力）から文字を読み込む。
+ */
 #include <xinu.h>
 #include <stdio.h>
 
-/*------------------------------------------------------------------------
- *  getchar  -  DOCUMENT
- *------------------------------------------------------------------------
+/**
+ * @brief STDIN（標準入力）から文字を読み込む。
+ * @return 成功時は読み込んだ文字、異常時はEOFを返す。
  */
-int	getchar(
-	  void
-	)
+int getchar(void)
 {
-    return fgetc(stdin);
+	return fgetc(stdin);
 }
