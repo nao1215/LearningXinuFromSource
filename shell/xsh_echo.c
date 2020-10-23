1730800d322a1f2ecb9262b15da904413fbe95da
Nao1215 <n.chika156@gmail.com>
2020-10-24 02:17:46 +0900

    追加：XINUシェルのechoコマンドに関するDoxygenコマンドを追加

--- shell/xsh_echo.c
+++ shell/xsh_echo.c
@@ -1,20 +1,27 @@
-/* xsh_echo.c - xsh_echo */
-
+/**
+ * @file xsh_echo.c
+ * @brief STDOUT（標準出力）に引数に指定した文字列を出力する。
+ */
 #include <xinu.h>
 #include <stdio.h>
 
-/*------------------------------------------------------------------------
- * xhs_echo - write argument strings to stdout
- *------------------------------------------------------------------------
+/**
+ * @brief STDOUT（標準出力）に引数に指定した文字列を出力する。
+ * @details 複数の引数がある場合は、改行せずにスペース区切りで引数が続けて出力される。
+ * @param[in] nargs 引数の数
+ * @param[in] args 引数（引数を持つ配列）
+ * @return 0を返す。
  */
 shellcmd xsh_echo(int nargs, char *args[])
 {
-	int32	i;			/* walks through args array	*/
+	int32 i; /* walks through args array	*/
 
-	if (nargs > 1) {
+	if (nargs > 1)
+	{
 		printf("%s", args[1]);
 
-		for (i = 2; i < nargs; i++) {
+		for (i = 2; i < nargs; i++)
+		{
 			printf(" %s", args[i]);
 		}
 	}
