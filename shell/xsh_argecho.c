618967c9ea0b82904ab5d61952c881ae5dd3277b
Nao1215 <n.chika156@gmail.com>
2020-10-24 01:55:52 +0900

    追加：XINUシェルのargechoコマンドに関するDoxygenコメントを追加

--- shell/xsh_argecho.c
+++ shell/xsh_argecho.c
@@ -1,18 +1,24 @@
-/* xsh_argecho.c - xsh_argecho */
-
+/**
+ * @file xsh_argecho.c
+ * @brief 引数を順に表示する。
+ */
 #include <xinu.h>
 #include <stdio.h>
 
-/*------------------------------------------------------------------------
- * xhs_argecho - display argecho message that lists shell commands
- *------------------------------------------------------------------------
+/**
+ * @brief 引数を順に表示する。
+ * @details 「引数番号」と「引数番号に対応する引数（文字列として扱う）」が1行単位で出力される。
+ * @param[in] nargs 引数の数
+ * @param[in] args 引数（引数を持つ配列）
+ * @return 0を返す。
  */
 shellcmd xsh_argecho(int nargs, char *args[])
 {
-	int32	i;
+	int32 i;
 
 	printf("\n\nThe %d arguments are:\n", nargs);
-	for (i = 0; i < nargs; i++) {
+	for (i = 0; i < nargs; i++)
+	{
 		printf("    %2d: %s\n", i, args[i]);
 	}
 	printf("\n");
