8c14fbc4d8db8497d538f7ad6f84fff136b763ed
Nao1215 <n.chika156@gmail.com>
2020-10-24 02:10:14 +0900

    追加：XINUシェルのclearコマンドに関するDoxygenコメントを追加

--- shell/xsh_clear.c
+++ shell/xsh_clear.c
@@ -1,20 +1,29 @@
-/* xsh_clear.c - xsh_clear */
-
+/**
+ * @file xsh_clear.c
+ * @brief ディスプレイウィンドウをクリアする（xtermやVT100を想定している）
+ */
 #include <xinu.h>
 #include <stdio.h>
 
-/*------------------------------------------------------------------------
- * xsh_clear - clear the display window (assumes xterm / VT100)
- *------------------------------------------------------------------------
+/**
+ * @brief  ディスプレイウィンドウをクリアする（xtermやVT100を想定している）
+ * @param[in] nargs 引数の数
+ * @param[in] args 引数（引数を持つ配列）
+ * @return 成功時は0、コマンド名以外の引数が存在する場合は1を返す。
+ * @note printf()で使用されているエスケープシーケンスは、以下の意味を持つ。<br>
+ * ・\033[0m ：エスケープシーケンス指定をリセットして、未指定状態に戻す。
+ * ・\033[2J\：画面クリアを行う。
+ * ・\033[H  ：カーソルを画面の左上隅（最初の行、最初の列）に移動させる。
  */
 shellcmd xsh_clear(int nargs, char *args[])
 {
 
     /* Insure no arguments were passed */
 
-    if (nargs > 1) {
-	fprintf(stderr,"use is: %s\n", args[0]);
-	return 1;
+    if (nargs > 1)
+    {
+        fprintf(stderr, "use is: %s\n", args[0]);
+        return 1;
     }
 
     printf("\033[0m\033[2J\033[H\n");
