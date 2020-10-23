7bbad76e2ffb3299d46fbcdd26951e8f5c2d06be
Nao1215 <n.chika156@gmail.com>
2020-10-23 21:18:42 +0900

    追加：gettime()に関するDoxygenコメントを追加

--- system/gettime.c
+++ system/gettime.c
@@ -1,22 +1,25 @@
-/* gettime.c - gettime */
-
+/**
+ * @file gettime.c
+ * @brief 1970年1月1日からの経過時間（秒単位）にロケーション（時差）を反映した時間を返す。
+ */
 #include <xinu.h>
 
-/*------------------------------------------------------------------------
- *  gettime  -  Get xinu time in seconds past Jan 1, 1970
- *------------------------------------------------------------------------
+/**
+ * @brief 1970年1月1日からの経過時間（秒単位）にロケーション（時差）を反映した時間を返す。
+ * @details UCT（協定世界時）からの経過時刻に対してロケーション（時差）を反映する。
+ * @param[in,out] timvar XINU時刻の取得結果の保存先
+ * @return XINU時間を取得できた場合はOK、UCTからの経過時間が取得できなかった場合はSYSERRを返す。
  */
-status	gettime(
-	  uint32  *timvar		/* Location to store the result	*/
-	)
+status gettime(uint32 *timvar)
 {
-	uint32	now;			/* Current time (UCT)		*/
-	int32	retval;			/* Return value from call	*/
+	uint32 now;	  /* Current time (UCT)		*/
+	int32 retval; /* Return value from call	*/
 
 	/* Get current time in UCT representation (GMT) */
 
 	retval = getutime(&now);
-	if (retval == SYSERR) {
+	if (retval == SYSERR)
+	{
 		return SYSERR;
 	}
 
