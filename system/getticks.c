cfbd322b0ff4f6c16618c829419b728705b89e6b
Nao1215 <n.chika156@gmail.com>
2020-10-23 16:16:08 +0900

    追加：gettick()に関するDoxygenコメントを追加

--- system/getticks.c
+++ system/getticks.c
@@ -1,14 +1,20 @@
-/* getticks.c - getticks */
-
+/**
+ * @file getticks.c
+ * @brief CPUリセット以降のclock tick数を取得する。
+ */
 #include <xinu.h>
 
-/*------------------------------------------------------------------------
- *  getticks  -  Retrieve the number of clock ticks since CPU reset
- *------------------------------------------------------------------------
+/**
+ * @brief CPUリセット以降のclock tick数を取得する。
+ * @return CPUリセット以降のclock tick
+ * @note インラインアセンブラ部分はPMCNT0〜PMCNT3レジスタを読み込んでいる。<br>
+ * なお、CP15（コプロセッサ15、p15）は、システム制御機能を提供する。<br>
+ * c9はパフォーマンスモニターカウントレジスタ、c13はサイクルカウントレジスタである。
  */
-uint32  	getticks()
+uint32 getticks()
 {
 	uint32 ret;
-        asm volatile ( "MRC p15, 0, %0, c9, c13, 0\t\n": "=r"(ret) );
+	asm volatile("MRC p15, 0, %0, c9, c13, 0\t\n"
+				 : "=r"(ret));
 	return ret;
 }
