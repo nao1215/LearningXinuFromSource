b4bd0f7c2d6cd0836654d657f2c49b46f835d4e2
Nao1215 <n.chika156@gmail.com>
2020-10-24 00:07:42 +0900

    追加：recvclr()、receive()に関するDoxygenコメントを追加

--- system/receive.c
+++ system/receive.c
@@ -1,25 +1,34 @@
-/* receive.c - receive */
-
+/**
+ * @file receive.c
+ * @brief メッセージの受信を待ち、受信後にreceive()の呼び出し者にメッセージを返す。
+ */
 #include <xinu.h>
 
-/*------------------------------------------------------------------------
- *  receive  -  Wait for a message and return the message to the caller
- *------------------------------------------------------------------------
+/**
+ * @brief メッセージの受信を待ち、受信後にreceive()の呼び出し者にメッセージを返す。
+ * @details
+ * Step1. 割り込みを禁止する。<br>
+ * Step2. 現在のプロセスが有効なメッセージを受信していない場合は受信待ち状態に移行し、<br>
+ * 再スケジューリングを行う。<br>
+ * Step3. プロセスが復帰後、メッセージを取得し、メッセージフラグをリセットする。<br>
+ * Step4. 割り込み状態を復元する。
+ * @return 取得したメッセージを返す。
  */
-umsg32	receive(void)
+umsg32 receive(void)
 {
-	intmask	mask;			/* Saved interrupt mask		*/
-	struct	procent *prptr;		/* Ptr to process's table entry	*/
-	umsg32	msg;			/* Message to return		*/
+	intmask mask;		   /* Saved interrupt mask		*/
+	struct procent *prptr; /* Ptr to process's table entry	*/
+	umsg32 msg;			   /* Message to return		*/
 
 	mask = disable();
 	prptr = &proctab[currpid];
-	if (prptr->prhasmsg == FALSE) {
+	if (prptr->prhasmsg == FALSE)
+	{
 		prptr->prstate = PR_RECV;
-		resched();		/* Block until message arrives	*/
+		resched(); /* Block until message arrives	*/
 	}
-	msg = prptr->prmsg;		/* Retrieve message		*/
-	prptr->prhasmsg = FALSE;	/* Reset message flag		*/
+	msg = prptr->prmsg;		 /* Retrieve message		*/
+	prptr->prhasmsg = FALSE; /* Reset message flag		*/
 	restore(mask);
 	return msg;
 }
--- system/recvclr.c
+++ system/recvclr.c
@@ -1,23 +1,31 @@
-/* recvclr.c - recvclr */
+/**
+ * @file recvclr.c
+ * @brief 受信メッセージをクリアし、待機している場合はメッセージを返す。
+ */
 
 #include <xinu.h>
 
-/*------------------------------------------------------------------------
- *  recvclr  -  Clear incoming message, and return message if one waiting
- *------------------------------------------------------------------------
+/**
+ * @brief 受信メッセージをクリアし、待機している場合はメッセージを返す。
+ * @return 現在のプロセスが有効なメッセージを持っている場合はその内容を返し、<br>
+ * 現在のプロセスが有効なメッセージを持っていない場合はOKを返す。
+ * @note receive()と異なり、メッセージ受信を待ち続けず、即座にリターンする。
  */
-umsg32	recvclr(void)
+umsg32 recvclr(void)
 {
-	intmask	mask;			/* Saved interrupt mask		*/
-	struct	procent *prptr;		/* Ptr to process's table entry	*/
-	umsg32	msg;			/* Message to return		*/
+	intmask mask;		   /* Saved interrupt mask		*/
+	struct procent *prptr; /* Ptr to process's table entry	*/
+	umsg32 msg;			   /* Message to return		*/
 
 	mask = disable();
 	prptr = &proctab[currpid];
-	if (prptr->prhasmsg == TRUE) {
-		msg = prptr->prmsg;	/* Retrieve message		*/
-		prptr->prhasmsg = FALSE;/* Reset message flag		*/
-	} else {
+	if (prptr->prhasmsg == TRUE)
+	{
+		msg = prptr->prmsg;		 /* Retrieve message		*/
+		prptr->prhasmsg = FALSE; /* Reset message flag		*/
+	}
+	else
+	{
 		msg = OK;
 	}
 	restore(mask);
