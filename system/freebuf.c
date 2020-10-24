8698850cecfca55881fbdd3608a4337cd78af0a1
Nao1215 <n.chika156@gmail.com>
2020-10-25 02:34:48 +0900

    追加：freebuf()に関するDoxygenコメントを追加

--- system/freebuf.c
+++ system/freebuf.c
@@ -1,18 +1,26 @@
-/* freebuf.c - freebuf */
-
+/**
+ * @file freebuf.c
+ * @brief バッファプールから取得したバッファを解放する。
+ */
 #include <xinu.h>
 
-/*------------------------------------------------------------------------
- *  freebuf  -  Free a buffer that was allocated from a pool by getbuf
- *------------------------------------------------------------------------
+/**
+ * @brief バッファプールから取得したバッファを解放する。
+ * @details
+ * Step1. 割り込みを禁止する。<br>
+ * Step2. 解放対象バッファアドレスの4Byte前に埋め込んだバッファプールIDが不正値かどうかを確認し、<br>
+ * 不正値であれば割り込み状態を復元し、処理を終了する。<br>
+ * Step3. 解放対象バッファをバッファプールに戻す。<br>
+ * Step4. セマフォにシグナルを送り、待機プロセスがいれば待機解除とする。<br>
+ * Step5. 割り込み状態を復元する。
+ * @param[in] bufaddr getbuf()で取得したバッファアドレス
+ * @return バッファを解放した場合はOK、バッファプールIDが不正値の場合はSYSERRを返す。
  */
-syscall	freebuf(
-	  char		*bufaddr	/* Address of buffer to return	*/
-	)
+syscall freebuf(char *bufaddr)
 {
-	intmask	mask;			/* Saved interrupt mask		*/
-	struct	bpentry	*bpptr;		/* Pointer to entry in buftab	*/
-	bpid32	poolid;			/* ID of buffer's pool		*/
+	intmask mask;		   /* Saved interrupt mask		*/
+	struct bpentry *bpptr; /* Pointer to entry in buftab	*/
+	bpid32 poolid;		   /* ID of buffer's pool		*/
 
 	mask = disable();
 
@@ -20,7 +28,8 @@ syscall	freebuf(
 
 	bufaddr -= sizeof(bpid32);
 	poolid = *(bpid32 *)bufaddr;
-	if (poolid < 0  ||  poolid >= nbpools) {
+	if (poolid < 0 || poolid >= nbpools)
+	{
 		restore(mask);
 		return SYSERR;
 	}
