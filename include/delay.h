db01a038de55522aa85bab69ee317f6018fb4396
Nao1215 <n.chika156@gmail.com>
2020-10-24 13:11:45 +0900

    追加：Delayに関するDoxygenコメントを追加

--- include/delay.h
+++ include/delay.h
@@ -1,25 +1,34 @@
-/* delay.h - DELAY */
-
-/*
- * Delay units are in microseconds.
+/**
+ * @file delay.h
+ * @brief ミリ秒もしくはマイクロ秒単位で処理を遅らせる。
  */
-#define	DELAY(n)                                	\
-{                                               	\
-	volatile long N = 2*n;				\
-							\
-	while(N > 0) {					\
-		N--;					\
-	}						\
-}
 
-/*
- * Delay units are in milli-seconds.
+/**
+ * @def DELAY(n)
+ * @param[in] n 遅延時間（マイクロ秒）
+ * @brief マイクロ秒単位で処理を遅らせる。
  */
-#define	MDELAY(n)					\
-{							\
-	register long i;				\
-							\
-	for (i=n;i>0;i--) {				\
-		DELAY(1000);				\
-	}						\
-}
+#define DELAY(n)                 \
+	{                            \
+		volatile long N = 2 * n; \
+                                 \
+		while (N > 0)            \
+		{                        \
+			N--;                 \
+		}                        \
+	}
+
+/**
+ * @def MDELAY(n)
+ * @param[in] n 遅延時間（ミリ秒）
+ * @brief ミリ秒単位で処理を遅らせる。
+ */
+#define MDELAY(n)               \
+	{                           \
+		register long i;        \
+                                \
+		for (i = n; i > 0; i--) \
+		{                       \
+			DELAY(1000);        \
+		}                       \
+	}
