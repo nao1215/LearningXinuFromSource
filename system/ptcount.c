684475cc8cbfac0bc0fdac2080d1a05efc274f43
Nao1215 <n.chika156@gmail.com>
2020-10-26 06:41:38 +0900

    追加：ptcount()に関するDoxygenコメントを追加

--- system/ptcount.c
+++ system/ptcount.c
@@ -1,30 +1,40 @@
-/* ptcount.c - ptcount */
-
+/**
+ * @file ptcount.c
+ * @brief ポートを用いたメッセージ送受信において、メッセージ数を数える。
+ */
 #include <xinu.h>
 
-/*------------------------------------------------------------------------
- *  ptcount  -  Return the count of messages currently waiting in a port
- *		  (a non-negative count K means the port contains K
- *		   K messages,  including messages from senders that
- *		   are blocked waiting to send a message; a count of
- *		   negative K means a port is empty and K processes
- *		   are blocked waiting to receive a message)
- *------------------------------------------------------------------------
+/**
+ * @brief ポートを用いたメッセージ送受信において、メッセージ数を数える。
+ * @details
+ * Step1. 割り込みを禁止する。<br>
+ * Step2. ポートIDが不正値もしくはポートが未割り当ての場合は、割り込み状態を復元し、<br>
+ * 処理を終了する。
+ * Step3. 受信メッセージ数（受信用セマフォ数）を数える。<br>
+ * Step4. 受信メッセージ数が0以上の場合は、送信メッセージ数（送信用セマフォ数）を数える。<br>
+ * Step5. 送信メッセージが0以下の場合は、受信メッセージ数にその数（送信者がブロックされた数）を<br>
+ * 加算する。<br>
+ * Step6. 割り込み状態を復元する。
+ * @param[in] portid ポートID
+ * @return 成功時は現在ポートで待機中のメッセージ数を返し、ポートIDが不正値もしくは<br>
+ * ポートが未割り当ての場合はSYSERRを返す。
+ * @note 正の値Kは、ポートにK個のメッセージ（送信者がブロックされて送信待機中のメッセージ含む）が<br>
+ * ある事を意味する。負の値Kはポートが空であり、K個のプロセスがメッセージ受信のために<br>
+ * 大気中である事を意味する。
  */
-int32	ptcount(
-	  int32		portid		/* ID of a port to use		*/
-	)
+int32 ptcount(int32 portid)
 {
-	intmask	mask;			/* Saved interrupt mask		*/
-	int32	count;			/* Count of messages available	*/
-	int32	sndcnt;			/* Count of sender semaphore	*/
-	struct	ptentry	*ptptr;		/* Pointer to port table entry	*/
+	intmask mask;		   /* Saved interrupt mask		*/
+	int32 count;		   /* Count of messages available	*/
+	int32 sndcnt;		   /* Count of sender semaphore	*/
+	struct ptentry *ptptr; /* Pointer to port table entry	*/
 
 	mask = disable();
-	if ( isbadport(portid) ||
-		(ptptr= &porttab[portid])->ptstate != PT_ALLOC ) {
-			restore(mask);
-			return SYSERR;
+	if (isbadport(portid) ||
+		(ptptr = &porttab[portid])->ptstate != PT_ALLOC)
+	{
+		restore(mask);
+		return SYSERR;
 	}
 
 	/* Get count of messages available */
@@ -33,9 +43,11 @@ int32	ptcount(
 
 	/* If messages are waiting, check for blocked senders */
 
-	if (count >= 0) {
+	if (count >= 0)
+	{
 		sndcnt = semcount(ptptr->ptssem);
-		if (sndcnt < 0) {	/* -sndcnt senders blocked */
+		if (sndcnt < 0)
+		{ /* -sndcnt senders blocked */
 			count += -sndcnt;
 		}
 	}
