506ab2a8c3a54ee3ebdb95192fb6044ae3f96a73
Nao1215 <n.chika156@gmail.com>
2020-10-25 16:00:26 +0900

    追加：putc()に関するDoxygenコメントを追加

--- system/putc.c
+++ system/putc.c
@@ -1,27 +1,34 @@
-/* putc.c - putc */
-
+/**
+ * @file putc.c
+ * @brief デバイスへ文字１Byteを送信する。
+ */
 #include <xinu.h>
 
-/*------------------------------------------------------------------------
- *  putc  -  Send one character of data (byte) to a device
- *------------------------------------------------------------------------
+/**
+ * @brief デバイスへ文字１Byteを送信する。
+ * @details
+ * Step1. 割り込みを禁止する。<br>
+ * Step2. デバイスディスクリプタが不正値の場合は、割り込み状態を復元し、処理を終了する。<br>
+ * Step3. デバイスに応じたputc()処理を行う。<br>
+ * Step4. 割り込み状態を復元する。
+ * @param[in] descrp デバイスディスクリプタ
+ * @param[in] ch 送信したい文字
+ * @return 成功時はデバイスに応じた返り値、デバイスディスクリプタが不正値の場合はSYSERRを返す。
  */
-syscall	putc(
-	  did32		descrp,		/* Descriptor for device	*/
-	  char		ch		/* Character to send		*/
-	)
+syscall putc(did32 descrp, char ch)
 {
-	intmask		mask;		/* Saved interrupt mask		*/
-	struct dentry	*devptr;	/* Entry in device switch table	*/
-	int32		retval;		/* Value to return to caller	*/
+	intmask mask;		   /* Saved interrupt mask		*/
+	struct dentry *devptr; /* Entry in device switch table	*/
+	int32 retval;		   /* Value to return to caller	*/
 
 	mask = disable();
-	if (isbaddev(descrp)) {
+	if (isbaddev(descrp))
+	{
 		restore(mask);
 		return SYSERR;
 	}
-	devptr = (struct dentry *) &devtab[descrp];
-	retval = (*devptr->dvputc) (devptr, ch);
+	devptr = (struct dentry *)&devtab[descrp];
+	retval = (*devptr->dvputc)(devptr, ch);
 	restore(mask);
 	return retval;
 }
