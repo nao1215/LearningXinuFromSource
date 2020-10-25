73ff95cc34c6b29403e418037ac7cf54c3301510
Nao1215 <n.chika156@gmail.com>
2020-10-25 13:15:43 +0900

    追加：ポートを用いたメッセージ構造体に関するDoxygenコメントを追加

--- include/ports.h
+++ include/ports.h
@@ -1,29 +1,64 @@
-/* ports.h - isbadport */
+/**
+ * @file ports.h
+ * @brief ポートを用いたメッセージ送受信に用いる構造体や定数の宣言
+ */
 
-#define	NPORTS		30		/* Maximum number of ports	*/
-#define	PT_MSGS		100		/* Total messages in system	*/
-#define	PT_FREE		1		/* Port is free			*/
-#define	PT_LIMBO	2		/* Port is being deleted/reset	*/
-#define	PT_ALLOC	3		/* Port is allocated		*/
+//! ポートの最大数
+#define NPORTS 30
+//! システム内の最大メッセージ数
+#define PT_MSGS 100
+//! ポートがFREE状態
+#define PT_FREE 1
+//! ポートが削除された、もしくはリセットされる
+#define PT_LIMBO 2
+//! ポートが割り当てられた
+#define PT_ALLOC 3
 
-struct	ptnode	{			/* Node on list of messages 	*/
-	uint32	ptmsg;			/* A one-word message		*/
-	struct	ptnode	*ptnext;	/* Pointer to next node on list	*/
+/**
+ * @struct ptnode
+ * @brief メッセージリストのノード
+ */
+struct ptnode
+{
+	//! ワンワードのメッセージ
+	uint32 ptmsg;
+	//! メッセージリストの次のノードへのポインタ
+	struct ptnode *ptnext;
 };
 
-struct	ptentry	{			/* Entry in the port table	*/
-	sid32	ptssem;			/* Sender semaphore		*/
-	sid32	ptrsem;			/* Receiver semaphore		*/
-	uint16	ptstate;		/* Port state (FREE/LIMBO/ALLOC)*/
-	uint16	ptmaxcnt;		/* Max messages to be queued	*/
-	int32	ptseq;			/* Sequence changed at creation	*/
-	struct	ptnode	*pthead;	/* List of message pointers	*/
-	struct	ptnode	*pttail;	/* Tail of message list		*/
+/**
+ * @struct ptentry
+ * @brief ポートテーブルエントリ
+ */
+struct ptentry
+{
+	//! 送信側セマフォ
+	sid32 ptssem;
+	//! 受信側セマフォ
+	sid32 ptrsem;
+	//! ポート状態（FREE／LIMBO／ALLOC）
+	uint16 ptstate;
+	//! ポートに挿入できる最大メッセージ数
+	uint16 ptmaxcnt;
+	//! 生成時に変更されたメッセージ
+	int32 ptseq;
+	//! メッセージリストの先頭ポインタ
+	struct ptnode *pthead;
+	//! メッセージリストの末尾ポインタ
+	struct ptnode *pttail;
 };
 
-extern	struct	ptnode	*ptfree;	/* List of free nodes		*/
-extern	struct	ptentry	porttab[];	/* Port table			*/
-extern	int32	ptnextid;		/* Next port ID to try when	*/
-					/*   looking for a free slot	*/
+//! フリーノードリストのextern宣言
+extern struct ptnode *ptfree;
+//! ポートテーブルエントリのextern宣言
+extern struct ptentry porttab[];
+//! 空きスロットを探す際に試みる次のポートID
+extern int32 ptnextid;
 
-#define	isbadport(portid)	( (portid)<0 || (portid)>=NPORTS )
+/**
+ * @def isbadport(portid)
+ * @brief ポートIDが不正値かどうかを確認する。
+ * @param[in] portid ポートID
+ * @return ポートIDが負の値もしくはポートの最大数を超えている場合はtrue、それ以外はfalseを返す。
+ */
+#define isbadport(portid) ((portid) < 0 || (portid) >= NPORTS)
