f02bef48804b34b0bd10325b2bd592070210ea96
Nao1215 <n.chika156@gmail.com>
2020-11-04 21:59:35 +0900

    追加：PAD制御レジスタに関する情報を追加

--- include/am335x_control.h
+++ include/am335x_control.h
@@ -1,9 +1,26 @@
-#define AM335X_PADCTL_PEN	0x00000008	/* Pull up-down enabled	*/
-#define AM335X_PADCTL_PUP	0x00000010	/* Pull up selected	*/
-#define AM335X_PADCTL_PDN	0x00000000	/* Pull down selected	*/
-#define AM335X_PADCTL_RXTX	0x00000020	/* Input or Output mode	*/
-#define AM335X_PADCTL_TX	0x00000000	/* Output only mode	*/
-#define AM335X_PADCTL_SS	0x00000040	/* Slew control slow	*/
-#define AM335X_PADCTL_SF	0x00000000	/* Slew control fast	*/
-
-#define	am335x_padctl(addr,val)	(*((uint32 *)(addr))) = (val)
+/**
+ * @file am335x_control.h
+ * @brief AM335X SoCのThe PAD制御レジスタに関する定義
+ * @details PAD制御レジスタ（32bit）は、各I/Oの信号多重化およびその他の側面を制御する。
+ */
+//! プルアップダウンが有効
+#define AM335X_PADCTL_PEN 0x00000008
+//! プルアップが選択された
+#define AM335X_PADCTL_PUP 0x00000010
+//! プルダウンが選択された
+#define AM335X_PADCTL_PDN 0x00000000
+//! 入力モードもしくは出力モード
+#define AM335X_PADCTL_RXTX 0x00000020
+//! 出力限定モード
+#define AM335X_PADCTL_TX 0x00000000
+//! スルーコントロールが遅い状態
+#define AM335X_PADCTL_SS 0x00000040
+//! スルーコントロールが速い状態
+#define AM335X_PADCTL_SF 0x00000000
+/**
+ * @def am335x_padctl()
+ * @brief 引数で指定されたアドレスに値を代入する。
+ * @param[in,out] addr 代入先のアドレス
+ * @param[in] val 代入する値（OR条件で指定されたBit値）
+ */
+#define am335x_padctl(addr, val) (*((uint32 *)(addr))) = (val)
