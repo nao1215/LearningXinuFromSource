d4712688c5ee895b71f45dbab846d6b9e2878cc4
Nao1215 <n.chika156@gmail.com>
2020-11-05 06:27:03 +0900

    追加：Watchdogタイマに関するDoxygenコメントを追加

--- include/am335x_watchdog.h
+++ include/am335x_watchdog.h
@@ -1,31 +1,55 @@
-/* am335x_watchdog.h */
+/**
+ * @file am335x_watchdog.h
+ * @brief AM335X SoCのWatchdogタイマの制御／状態レジスタに関する定義
+ */
 
-/* Control and Status registers for watchdog timer */
-
-struct	watchdog_csreg {
-	volatile uint32 widr;	/* Identification register	*/
-	volatile uint32 res1[3];/* Reserved space		*/
-	volatile uint32	wdsc;	/* System control register	*/
-	volatile uint32	wdst;	/* Status register		*/
-	volatile uint32	wisr;	/* Interrupt status register	*/
-	volatile uint32	wier;	/* Interrupt enable register	*/
-	volatile uint32	res2;	/* Reserved space		*/
-	volatile uint32	wclr;	/* Control register		*/
-	volatile uint32	wcrr;	/* Counter register		*/
-	volatile uint32	wldr;	/* Load register		*/
-	volatile uint32	wtgr;	/* Trigger register		*/
-	volatile uint32	wwps;	/* Write posting bits register	*/
-	volatile uint32	res3[3];/* Reserved space		*/
-	volatile uint32	wdly;	/* Delay configuration register	*/
-	volatile uint32	wspr;	/* Start/stop register		*/
-	volatile uint32	res4[2];/* Reserved space		*/
-	volatile uint32	wirqstatraw;
-				/* Raw interrupt status register*/
-	volatile uint32	wirqstat;/* Interrupt status register	*/
-	volatile uint32	wirqenset;
-				/* Interrupt enable set register*/
-	volatile uint32	wirqenclr;
-				/* Interrupt enable clr register*/
+/**
+ * @struct watchdog_csreg
+ * @brief Watchdog CSREG（制御／状態）レジスタ操作用の構造体
+ */
+struct watchdog_csreg
+{
+	//! 識別レジスタ
+	volatile uint32 widr;
+	//! 予約領域その1
+	volatile uint32 res1[3];
+	//! システム制御レジスタ
+	volatile uint32 wdsc;
+	//! ステータスレジスタ
+	volatile uint32 wdst;
+	//! 割り込みステータスレジスタ
+	volatile uint32 wisr;
+	//! 割り込み許可レジスタ
+	volatile uint32 wier;
+	//! 予約領域その2
+	volatile uint32 res2;
+	//! 制御レジスタ
+	volatile uint32 wclr;
+	//! カウンタレジスタ
+	volatile uint32 wcrr;
+	//! ロードレジスタ
+	volatile uint32 wldr;
+	//! トリガーレジスタ
+	volatile uint32 wtgr;
+	//! ポストBitレジスタの書き込み
+	volatile uint32 wwps;
+	//! 予約領域その3
+	volatile uint32 res3[3];
+	//! 遅延制御レジスタ
+	volatile uint32 wdly;
+	//! 開始／終了レジスタ
+	volatile uint32 wspr;
+	//! 予約領域その4
+	volatile uint32 res4[2];
+	//! Raw割り込み開始レジスタ
+	volatile uint32 wirqstatraw;
+	//! 割り込みステータスレジスタ
+	volatile uint32 wirqstat;
+	//! 割り込み許可セットレジスタ
+	volatile uint32 wirqenset;
+	//! 割り込み許可コントロールレジスタ
+	volatile uint32 wirqenclr;
 };
 
-#define	WDTADDR	0x44E35000
+//! Watchdogタイマに割り当てられたアドレス
+#define WDTADDR 0x44E35000
